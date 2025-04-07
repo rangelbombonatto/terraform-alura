a4eac6e62a76775e3d2f10cc4e44041cc5897c01
Rangel Luiz Bombonatto <rangel-16@hotmail.com>
2025-04-07 13:13:17 -0300

    implemntação no ansible

--- main.tf
+++ main.tf
@@ -17,8 +17,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-075686beab831bb7f"
   instance_type = "t2.micro"
   key_name = "iac-alura"
-  
+  # user_data = <<-EOF
+  #                #!/bin/bash
+  #                cd /home/ubuntu
+  #                echo "<h1>Feito com Terraform 2</h1>" > index.html
+  #                nohup busybox httpd -f -p 8080 &
+  #                EOF
+
   tags = {
-    Name = "Primeira instancia"
+    Name = "Teste AWS"
   }
 }
